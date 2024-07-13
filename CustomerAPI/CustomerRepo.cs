6f45707911387a896eac3ed154fa311ed3ac85d5
student-AVD <student-AVD@ML-RefVm-842662>
2024-07-12 22:34:37 -0600

    Add project files.

---
+++ CustomerAPI/CustomerRepo.cs
@@ -0,0 +1,16 @@
+﻿
+namespace CustomerAPI
+{
+    public class CustomerRepo : ICustomerRepo
+    {
+        public List<Customer> GetAllCustomers()
+        {
+            return new List<Customer>
+        {
+            new Customer { Id = 1, FirstName = "Akarsh", LastName = "ptl" },
+            new Customer { Id = 2, FirstName = "aka", LastName = "ptl" },
+            
+        };
+        }
+    }
+}
